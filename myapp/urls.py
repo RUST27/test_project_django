0c4fd1cd97c8afa28ff398589f72c27fb026e64d
RUST27 <teranjuarezcristian@gmail.com>
2025-05-30 16:52:50 -0600

    params y models llamado a la base de datos desde views pasando parametros con models

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Hello),
+    path('', views.hello),
+    path('hello/<str:username>', views.hello),
+    path('projects/', views.projects),
+    path('tasks/<int:id>', views.tasks),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
-def Hello(request):
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls page.")
+def hello(request, username):
     return HttpResponse('<h1>Hello World</h1>')
+
+def projects(request):
+    projects1 = list(Project.objects.values())
+    return JsonResponse(projects1, safe=False)
+
+def tasks(request, id):
+    #task = Task.objects.get(id=id)
+    task = get_object_or_404(Task, id=id)
+    return HttpResponse('task: %s' % task.title)
 
 
